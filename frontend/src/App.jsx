3f5ac6f513875bed3698d3ebf4b80650e0ea894b
sakshisrivastava1 <sakshisrivas33@gmail.com>
2026-02-02 18:56:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Navigate, Route, Routes, useLocation } from 'react-router-dom'
 import Signup from './pages/Signup'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import Nav from './component/Nav'
+import Nav from './components/Nav'
 import { userDataContext } from './context/UserContext'
 import About from './pages/About'
 import Collections from './pages/Collections'
@@ -15,7 +15,7 @@ import PlaceOrder from './pages/PlaceOrder'
 import Order from './pages/Order'
 import { ToastContainer } from 'react-toastify';
 import NotFound from './pages/NotFound'
-import Ai from './component/Ai'
+import Ai from './components/Ai'
 function App() {
 let {userData} = useContext(userDataContext)
 let location = useLocation()
