7bac13713459b1ee8339a1760d253a29dc1d030e
sakshisrivastava1 <sakshisrivas33@gmail.com>
2026-02-02 18:40:28 +0530

    Refactor root route to use middleware

--- backend/index.js
+++ backend/index.js
@@ -39,9 +39,10 @@ app.use('/api/cart', cartRouter)
 
 app.use(express.static(path.join(__dirname, "dist")))
 
-app.get("/", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"))
 })
+
 
 app.listen(port, () =>
   console.log(`Server running on port ${port}`)
