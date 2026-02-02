1c5e9f8bc68cf7cc1515ee8620a4b87e12f7f622
sakshisrivastava1 <sakshisrivas33@gmail.com>
2026-02-02 18:23:00 +0530

    Refactor index.js for improved structure and clarity

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,48 @@
 import express from 'express'
 import dotenv from 'dotenv'
-dotenv.config()
-import { connectToDb } from './config/db.js'
-connectToDb()
-import cookieParser from "cookie-parser";
+import path from "path"
+import cookieParser from "cookie-parser"
 import cors from 'cors'
+
+import { connectToDb } from './config/db.js'
 import authRouter from './routes/authRoute.js'
 import userRouter from './routes/userRoute.js'
 import productRouter from './routes/productRoute.js'
 import orderRouter from './routes/orderRoute.js'
 import cartRouter from './routes/cartRoute.js'
+
+dotenv.config()
+connectToDb()
+
 const app = express()
-
 const port = process.env.PORT || 4000
+const __dirname = path.resolve()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
 app.use(cors({
-    origin:["https://onecart-frontend-aob0.onrender.com","https://onecart-admin-vv0e.onrender.com"],
-    credentials:true
-}));
+  origin: [
+    "https://onecart-frontend-aob0.onrender.com",
+    "https://onecart-admin-vv0e.onrender.com"
+  ],
+  credentials: true
+}))
 
-app.use('/api/auth',authRouter)
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
+
+app.use('/api/auth', authRouter)
+app.use('/api/user', userRouter)
+app.use('/api/product', productRouter)
 app.use('/api/order', orderRouter)
 app.use('/api/cart', cartRouter)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static(path.join(__dirname, "dist")))
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"))
+})
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () =>
+  console.log(`Server running on port ${port}`)
+)
