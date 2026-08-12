b71761c211c3684c989fb0e5569a9d0d17fb484e
Sakshi Srivastava <sakshisrivas33@gmail.com>
2026-08-12 13:55:56 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://onecart-89c5.onrender.com" , "https://onecart-admin-25xu.onrender.com"],
  credentials:true
 }))
 
