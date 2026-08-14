020fd07bd86195db49e0b13dba67f0a2b052b6e9
Sakshi Srivastava <sakshisrivas33@gmail.com>
2026-08-14 12:33:16 +0530

    lo

--- client/src/component/Nav.jsx
+++ client/src/component/Nav.jsx
@@ -25,6 +25,7 @@ function Nav() {
             const result = await axios.get(serverUrl + "/api/auth/logout" , {withCredentials:true})
             console.log(result.data)
             setUserData(null)
+            localStorage.removeItem("userData")
             navigate("/login")
         } catch (error) {
             console.log(error)
